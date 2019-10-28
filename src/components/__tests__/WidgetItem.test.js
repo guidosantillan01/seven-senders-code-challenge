5532d3f1c05227ccac429ba7c227ed97c35532e0
Guido Santillan <guidosantillan01@gmail.com>
2019-10-28 12:54:16 +0100

    Final commit

--- src/components/__tests__/WidgetItem.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WidgetItem from '../WidgetItem';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<WidgetItem />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
